94acbc0b73295af739b2191c0350ae50b2a8d8a5
Will Tachau <wtachau@gmail.com>
2016-02-04 00:37:02 -0800

    motherfucking trailing slash pt 2

--- src/main/scala/api/TweetHttpService.scala
+++ src/main/scala/api/TweetHttpService.scala
@@ -46,147 +46,149 @@ trait TweetHttpService extends HttpService
             }
           }
         } ~
-        path("recentTweets") {
-          parameters("candidate") { candidate =>
-            get {
-              complete {
-                candidate match {
-                  case "bernie" =>
-                    Map(
-                      "id" -> s"${RecentStatusRecorder.recentBernieStatus.getId}",
-                      "user" -> RecentStatusRecorder.recentBernieStatus.getUser.getScreenName
-                    ).toJson.toString
-                  case "clinton" =>
-                    Map(
-                      "id" -> s"${RecentStatusRecorder.recentClintonStatus.getId}",
-                      "user" -> RecentStatusRecorder.recentClintonStatus.getUser.getScreenName
-                    ).toJson.toString
-                  case "trump" =>
-                    Map(
-                      "id" -> s"${RecentStatusRecorder.recentTrumpStatus.getId}",
-                      "user" -> RecentStatusRecorder.recentTrumpStatus.getUser.getScreenName
-                    ).toJson.toString
-                  case "cruz" =>
-                    Map(
-                      "id" -> s"${RecentStatusRecorder.recentCruzStatus.getId}",
-                      "user" -> RecentStatusRecorder.recentCruzStatus.getUser.getScreenName
-                    ).toJson.toString
-                  case "rubio" =>
-                    Map(
-                      "id" -> s"${RecentStatusRecorder.recentRubioStatus.getId}",
-                      "user" -> RecentStatusRecorder.recentRubioStatus.getUser.getScreenName
-                    ).toJson.toString
-                  case _ => "candidate not found"
+        pathPrefix("recentTweets") {
+          pathSingleSlash {
+            parameters("candidate") { candidate =>
+              get {
+                complete {
+                  candidate match {
+                    case "bernie" =>
+                      Map(
+                        "id" -> s"${RecentStatusRecorder.recentBernieStatus.getId}",
+                        "user" -> RecentStatusRecorder.recentBernieStatus.getUser.getScreenName
+                      ).toJson.toString
+                    case "clinton" =>
+                      Map(
+                        "id" -> s"${RecentStatusRecorder.recentClintonStatus.getId}",
+                        "user" -> RecentStatusRecorder.recentClintonStatus.getUser.getScreenName
+                      ).toJson.toString
+                    case "trump" =>
+                      Map(
+                        "id" -> s"${RecentStatusRecorder.recentTrumpStatus.getId}",
+                        "user" -> RecentStatusRecorder.recentTrumpStatus.getUser.getScreenName
+                      ).toJson.toString
+                    case "cruz" =>
+                      Map(
+                        "id" -> s"${RecentStatusRecorder.recentCruzStatus.getId}",
+                        "user" -> RecentStatusRecorder.recentCruzStatus.getUser.getScreenName
+                      ).toJson.toString
+                    case "rubio" =>
+                      Map(
+                        "id" -> s"${RecentStatusRecorder.recentRubioStatus.getId}",
+                        "user" -> RecentStatusRecorder.recentRubioStatus.getUser.getScreenName
+                      ).toJson.toString
+                    case _ => "candidate not found"
+                  }
                 }
               }
             }
-          }
-        } ~
-        pathPrefix("pastTweets") {
-          pathSingleSlash {
-            get {
-              onComplete(dayRecordService.getAllRecords()) {
-                case Success(records) =>
-                  // Group records by the day
-                  val recordDayMaps = records.groupBy(record =>
-                    record.data.date.getDayOfYear).map {
-                    case (day, recordGroup) =>
-                      val futureRecords: Seq[Set[Future[Option[WithId[model.CandidateRecord]]]]] = recordGroup map {
-                        dayRecord => dayRecord.data.candidateRecords.map {
-                          candidateRecordID => candidateRecordService.findById(candidateRecordID)
-                        }
-                      }
-                      // Do weird future sequencing stuff
-                      val futureCandidateRecords: Seq[Future[Set[Option[WithId[model.CandidateRecord]]]]] = futureRecords.map {
-                        candidateRecords => Future.sequence(candidateRecords)
-                      }
-                      val futureDayRecords: Future[Seq[Set[Option[WithId[model.CandidateRecord]]]]] = Future.sequence(futureCandidateRecords)
-
-                      // Split up
-                      def unpackTotalAndAverage(list: List[WithId[model.CandidateRecord]]): Tuple2[Int, Double] = {
-                        list.foldLeft((0, 0.0))((a: Tuple2[Int, Double], b) => {
-                          val newTotal = a._1 + b.data.total
-                          val newAverage: Double = newTotal match {
-                            case 0 => 0.0
-                            case i: Int =>
-                              logger.info(s"\n\n$a, $b")
-                              ((a._2 * a._1 + b.data.total * b.data.average) / i)
-
-                          }
-                          (newTotal, newAverage)
-                        })
-                      }
-
-                      var bernieRecords = new ListBuffer[WithId[model.CandidateRecord]]
-                      var trumpRecords = new ListBuffer[WithId[model.CandidateRecord]]
-                      var clintonRecords = new ListBuffer[WithId[model.CandidateRecord]]
-                      var cruzRecords = new ListBuffer[WithId[model.CandidateRecord]]
-                      var rubioRecords = new ListBuffer[WithId[model.CandidateRecord]]
-
-                      Await.result(futureDayRecords, 20 seconds).toList.map { set =>
-                        set map { recordOption =>
-                          recordOption map { record =>
-                            record.data.candidate match {
-                              case Candidate.Bernie =>
-                                bernieRecords += record
-                              case Candidate.Trump =>
-                                trumpRecords += record
-                              case Candidate.Clinton =>
-                                clintonRecords += record
-                              case Candidate.Cruz =>
-                                cruzRecords += record
-                              case Candidate.Rubio =>
-                                rubioRecords += record
+          } ~
+            pathPrefix("pastTweets") {
+              pathSingleSlash {
+                get {
+                  onComplete(dayRecordService.getAllRecords()) {
+                    case Success(records) =>
+                      // Group records by the day
+                      val recordDayMaps = records.groupBy(record =>
+                        record.data.date.getDayOfYear).map {
+                        case (day, recordGroup) =>
+                          val futureRecords: Seq[Set[Future[Option[WithId[model.CandidateRecord]]]]] = recordGroup map {
+                            dayRecord => dayRecord.data.candidateRecords.map {
+                              candidateRecordID => candidateRecordService.findById(candidateRecordID)
                             }
                           }
-                        }
+                          // Do weird future sequencing stuff
+                          val futureCandidateRecords: Seq[Future[Set[Option[WithId[model.CandidateRecord]]]]] = futureRecords.map {
+                            candidateRecords => Future.sequence(candidateRecords)
+                          }
+                          val futureDayRecords: Future[Seq[Set[Option[WithId[model.CandidateRecord]]]]] = Future.sequence(futureCandidateRecords)
+
+                          // Split up
+                          def unpackTotalAndAverage(list: List[WithId[model.CandidateRecord]]): Tuple2[Int, Double] = {
+                            list.foldLeft((0, 0.0))((a: Tuple2[Int, Double], b) => {
+                              val newTotal = a._1 + b.data.total
+                              val newAverage: Double = newTotal match {
+                                case 0 => 0.0
+                                case i: Int =>
+                                  logger.info(s"\n\n$a, $b")
+                                  ((a._2 * a._1 + b.data.total * b.data.average) / i)
+
+                              }
+                              (newTotal, newAverage)
+                            })
+                          }
+
+                          var bernieRecords = new ListBuffer[WithId[model.CandidateRecord]]
+                          var trumpRecords = new ListBuffer[WithId[model.CandidateRecord]]
+                          var clintonRecords = new ListBuffer[WithId[model.CandidateRecord]]
+                          var cruzRecords = new ListBuffer[WithId[model.CandidateRecord]]
+                          var rubioRecords = new ListBuffer[WithId[model.CandidateRecord]]
+
+                          Await.result(futureDayRecords, 20 seconds).toList.map { set =>
+                            set map { recordOption =>
+                              recordOption map { record =>
+                                record.data.candidate match {
+                                  case Candidate.Bernie =>
+                                    bernieRecords += record
+                                  case Candidate.Trump =>
+                                    trumpRecords += record
+                                  case Candidate.Clinton =>
+                                    clintonRecords += record
+                                  case Candidate.Cruz =>
+                                    cruzRecords += record
+                                  case Candidate.Rubio =>
+                                    rubioRecords += record
+                                }
+                              }
+                            }
+                          }
+
+                          val (bernieTotal, bernieAverage) = unpackTotalAndAverage(bernieRecords.toList)
+                          val (trumpTotal, trumpAverage) = unpackTotalAndAverage(trumpRecords.toList)
+                          val (clintonTotal, clintonAverage) = unpackTotalAndAverage(clintonRecords.toList)
+                          val (cruzTotal, cruzAverage) = unpackTotalAndAverage(cruzRecords.toList)
+                          val (rubioTotal, rubioAverage) = unpackTotalAndAverage(rubioRecords.toList)
+
+                          PlayJson.obj(
+
+                            s"$day" -> PlayJson.obj(
+                              "bernie" -> PlayJson.obj(
+                                "total" -> bernieTotal,
+                                "average" -> bernieAverage
+                              ),
+                              "clinton" -> PlayJson.obj(
+                                "total" -> clintonTotal,
+                                "average" -> clintonAverage
+                              ),
+                              "trump" -> PlayJson.obj(
+                                "total" -> trumpTotal,
+                                "average" -> trumpAverage
+                              ),
+                              "cruz" -> PlayJson.obj(
+                                "total" -> cruzTotal,
+                                "average" -> cruzAverage
+                              ),
+                              "rubio" -> PlayJson.obj(
+                                "total" -> rubioTotal,
+                                "average" -> rubioAverage
+                              )
+                            )
+                          )
+                      }
+                      complete {
+                        PlayJson.obj("dayRecords" -> recordDayMaps.toList).toString()
                       }
 
-                      val (bernieTotal, bernieAverage) = unpackTotalAndAverage(bernieRecords.toList)
-                      val (trumpTotal, trumpAverage) = unpackTotalAndAverage(trumpRecords.toList)
-                      val (clintonTotal, clintonAverage) = unpackTotalAndAverage(clintonRecords.toList)
-                      val (cruzTotal, cruzAverage) = unpackTotalAndAverage(cruzRecords.toList)
-                      val (rubioTotal, rubioAverage) = unpackTotalAndAverage(rubioRecords.toList)
-
-                      PlayJson.obj(
-
-                        s"$day" -> PlayJson.obj(
-                          "bernie" -> PlayJson.obj(
-                            "total" -> bernieTotal,
-                            "average" -> bernieAverage
-                          ),
-                          "clinton" -> PlayJson.obj(
-                            "total" -> clintonTotal,
-                            "average" -> clintonAverage
-                          ),
-                          "trump" -> PlayJson.obj(
-                            "total" -> trumpTotal,
-                            "average" -> trumpAverage
-                          ),
-                          "cruz" -> PlayJson.obj(
-                            "total" -> cruzTotal,
-                            "average" -> cruzAverage
-                          ),
-                          "rubio" -> PlayJson.obj(
-                            "total" -> rubioTotal,
-                            "average" -> rubioAverage
-                          )
-                        )
-                      )
+                    case Failure(ex) =>
+                      complete {
+                        print(s">> Error getting record")
+                        ex.printStackTrace()
+                        "ruh roh"
+                      }
                   }
-                  complete {
-                    PlayJson.obj("dayRecords" -> recordDayMaps.toList).toString()
-                  }
-
-                case Failure(ex) =>
-                  complete {
-                    print(s">> Error getting record")
-                    ex.printStackTrace()
-                    "ruh roh"
-                  }
+                }
               }
             }
-          }
         }
       }
     }
